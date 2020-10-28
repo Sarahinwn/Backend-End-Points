ea004222cd3c7e0de79f6213ea92043efb922732
Judith Sarahi Rivas Estrada <lrs17110336@purisima.tecnm.mx>
2020-10-27 22:03:42 -0600

    end-points finish

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+//Alumnos
+const app = express();
+app.use(cors({
+    origin: true
+}));
+app.use(express.json());
+
+app.use('/api/alumnos', require('./routes/alumnos'));
+app.use('/api/docentes', require('./routes/docentes'));
+app.use('/api/materias', require('./routes/materias'));
+app.listen(process.env.PORT);
