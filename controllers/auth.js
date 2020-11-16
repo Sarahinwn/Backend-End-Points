1d10d0bd59691a6f63a9bdc73be1a664f86ae3fd
Judith Sarahi Rivas Estrada <lrs17110336@purisima.tecnm.mx>
2020-11-16 13:18:50 -0600

    Authentification Google part2

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,16 +61,69 @@ const login = async(req, res = response) => {
 
 const googleSignIn = async(req, res = response) => {
     const googleToken = req.body.token;
+    let usuario = null;
 
     try {
         const { name, email, picture } = await googleVerify(googleToken);
+
+        sql.on('error', err => {
+            console.log(err);
+            res.json({
+                ok: false,
+                error: err
+            });
+
+        });
+
+        //login
+        await sql.connect(conString).then(pool => {
+            return pool.request()
+                .input('email', email)
+                .execute('stp_usuarios_login');
+        }).then(result => {
+            usuario = result.recordset[0];
+        }).catch(err => {
+            usuario = null;
+        });
+        //verificasr si existe en BD
+        if (!usuario) {
+            //crear usuario
+            await sql.connect(conString).then(pool => {
+                return pool.request()
+                    .input('nombre', name)
+                    .input('email', email)
+                    .input('google', 1)
+                    .input('imagen', picture)
+                    .execute('stp_usuarios_add');
+            }).then(result => {
+                console.log('usuario de google creado')
+            }).catch(err => {
+                console.log('No se pudo crear usuario google')
+            });
+        } else {
+            //actualizar usuario
+            await sql.connect(conString).then(pool => {
+                return pool.request()
+                    .input('idUsuario', usuario.idUsuario)
+                    .input('nombre', name)
+                    .input('email', email)
+                    .input('google', 1)
+                    .input('imagen', picture)
+                    .execute('stp_usuarios_update');
+            }).then(result => {
+                console.log('usuario de google actualizado')
+            }).catch(err => {
+                console.log('No se pudo actualizar usuario google')
+            });
+        }
+
+        const googleToken = req.body.token;
+
         res.json({
             ok: true,
-            msg: 'Logeado correctamente',
-            name,
-            email,
-            picture
-        })
+            token: token
+        });
+
     } catch (error) {
         res.status(401).json({
             ok: false,
